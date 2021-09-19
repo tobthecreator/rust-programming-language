8b569955b47116115f2e7103da45baaf831ea153
Tyler O'Briant <tylerobriant@gmail.com>
2021-09-19 15:32:35 -0500

    chapter one

---
+++ chapter-one/hello-cargo/src/main.rs
@@ -0,0 +1,8 @@
+// cargo build builds
+// cargo run builds and then executes
+// cargo check checks for compile errors
+// cargo build --release to compile with optimization
+
+fn main() {
+    println!("Hello, tob 2!");
+}
